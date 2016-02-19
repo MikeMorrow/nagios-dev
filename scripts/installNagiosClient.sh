3e046e01712ceb6a00c5c9657d0f0d2e2adfbf74
Mike Morrow <mmorrow@vt.edu>
2016-02-18 22:43:36 -0500

    Continued setup. Still not working.

---
+++ scripts/installNagiosClient.sh
@@ -0,0 +1,3 @@
+yum update -y
+yum install epel-release -y
+yum install nagios-plugins-all -y
--- scripts/installNagiosServer.sh
+++ scripts/installNagiosServer.sh
@@ -1,5 +1,6 @@
-yum update
+yum update -y
 yum install epel-release -y
+#
 export modPythonSource=http://dist.modpython.org/dist/mod_python-3.5.0.tgz
 # ---------------------------------------------------------------------------
 # Install httpd
@@ -9,18 +10,21 @@ systemctl start httpd
 systemctl enable httpd
 #firewall-cmd --permanent --add-service=http
 #systemctl restart firewalld
-yum install httpd-devel python-devel -y
-yum groupinstall "Development Tools" -y
-cd /tmp
-curl -L -O ${modPythonSource}
-tar zxf mod_python-*.tgz
-cd mod_python-*
-./configure -with-apxs=/usr/bin/apxs
-patch ./src/Makefile /vagrant/conf/mod_python/Makefile.patch
-patch ./dist/version.sh /vagrant/conf/mod_python/version.sh.patch
-make
-make install
-cp /vagrant/conf/httpd/01-mod_python.conf /etc/httpd/conf.modules.d/
+# ---------------------------------------------------------------------------
+# Install httpd with mod_python
+# ---------------------------------------------------------------------------
+# yum install httpd httpd-devel python-devel -y
+# yum groupinstall "Development Tools" -y
+# cd /tmp
+# curl -L -O ${modPythonSource}
+# tar zxf mod_python-*.tgz
+# cd mod_python-*
+# ./configure -with-apxs=/usr/bin/apxs
+# patch ./src/Makefile /vagrant/conf/mod_python/Makefile.patch
+# patch ./dist/version.sh /vagrant/conf/mod_python/version.sh.patch
+# make
+# make install
+# cp /vagrant/conf/httpd/01-mod_python.conf /etc/httpd/conf.modules.d/
 # ---------------------------------------------------------------------------
 # Install mariadb
 # ---------------------------------------------------------------------------
@@ -38,10 +42,19 @@ systemctl restart httpd
 # Install nagios
 # ---------------------------------------------------------------------------
 yum install nagios -y
-yum install check-mk-agent check-mk check-mk-multisite -y
 echo "vagrant" | htpasswd -i /etc/nagios/passwd nagiosadmin
-sed -i "s|/etc/nagios/htpasswd.users|/etc/nagios/passwd|" /etc/httpd/conf.d/zzz_check_mk.conf
 systemctl restart nagios
+# ---------------------------------------------------------------------------
+# Install check_mk
+# ---------------------------------------------------------------------------
+yum install check-mk-agent check-mk check-mk-multisite -y
+sed -i "s|/etc/nagios/htpasswd.users|/etc/nagios/passwd|" /etc/httpd/conf.d/zzz_check_mk.conf
+# ---------------------------------------------------------------------------
+# Install nrpe
+# ---------------------------------------------------------------------------
+yum install nrpe nagios-plugins-nrpe -y
+systemctl restart nrpe
+systemctl enable nrpe
 # ---------------------------------------------------------------------------
 # Install base plugins
 # ---------------------------------------------------------------------------
@@ -54,17 +67,17 @@ yum install nagios-plugins-all -y
 # ---------------------------------------------------------------------------
 # Fix check_mk errors.... still broken
 # ---------------------------------------------------------------------------
-echo "rrd_path		    = '/var/lib/check_mk/rrd' " >> /usr/share/check_mk/web/htdocs/defaults.py
-mkdir -p /var/lib/check_mk/rrd
-chown -R apache.nagios /var/lib/check_mk
-#chmod 755 /var/lib/check_mk/*
-chcon --reference /var/lib/check_mk/autochecks /var/lib/check_mk/rrd
-systemctl restart httpd
-chmod 770 /etc/check_mk/conf.d/wato/
-chown -R apache.nagios /etc/check_mk/
-sed -i "s|/etc/nagios/htpasswd.users|/etc/nagios/passwd|" /usr/share/check_mk/modules/defaults
-sed -i "s|/etc/nagios/htpasswd.users|/etc/nagios/passwd|" /usr/share/check_mk/web/htdocs/defaults.py
-chmod 644 /etc/nagios/passwd
-mkdir /var/log/nagios/rw
-touch /var/log/nagios/rw/live
-chown -R nagios.nagios /var/log/nagios/rw/
+# echo "rrd_path		    = '/var/lib/check_mk/rrd' " >> /usr/share/check_mk/web/htdocs/defaults.py
+# mkdir -p /var/lib/check_mk/rrd
+# chown -R apache.nagios /var/lib/check_mk
+# #chmod 755 /var/lib/check_mk/*
+# chcon --reference /var/lib/check_mk/autochecks /var/lib/check_mk/rrd
+# systemctl restart httpd
+# chmod 770 /etc/check_mk/conf.d/wato/
+# chown -R apache.nagios /etc/check_mk/
+# sed -i "s|/etc/nagios/htpasswd.users|/etc/nagios/passwd|" /usr/share/check_mk/modules/defaults
+# sed -i "s|/etc/nagios/htpasswd.users|/etc/nagios/passwd|" /usr/share/check_mk/web/htdocs/defaults.py
+# chmod 644 /etc/nagios/passwd
+# mkdir /var/log/nagios/rw
+# touch /var/log/nagios/rw/live
+# chown -R nagios.nagios /var/log/nagios/rw/
