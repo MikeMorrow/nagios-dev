1a7cd45a44a196165cf0b170f2eda8e61930159a
Mike Morrow <mmorrow@vt.edu>
2016-02-23 21:32:03 -0500

    Add check_mk inventory of checks, precompile, configure, and restart core for client and nagiosserver.

--- scripts/installOMD.sh
+++ scripts/installOMD.sh
@@ -7,33 +7,12 @@ service xinetd restart
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get install gdebi-core -y
-wget http://files.omdistro.org/releases/debian_ubuntu/omd-1.20.trusty.amd64.deb
+wget --quiet http://files.omdistro.org/releases/debian_ubuntu/omd-1.20.trusty.amd64.deb
 gdebi -n omd-1.20.trusty.amd64.deb
 a2enmod proxy_http
 omd create dev
 su - dev -c "omd start"
 /bin/cp /vagrant/conf/check_mk/main.mk /opt/omd/sites/dev/etc/check_mk
 /etc/init.d/apache2 restart
-
-
-
-
-
-
-# ---------------------------------------------------------------------------
-# Install mariadb
-# ---------------------------------------------------------------------------
-#export DEBIAN_FRONTEND=noninteractive
-#apt-get install mariadb-server -y
-#echo -e "\n\nsecret\nsecret\n\n\nn\n\n " | mysql_secure_installation 2>/dev/null
-
-
-# wget http://files.omdistro.org/releases/debian_ubuntu/omd-1.20.trusty.amd64.deb
-# export DEBIAN_FRONTEND=noninteractive
-# apt-get install dialog xinetd build-essential apache2 libapache2-mod-python
-# apt-get install nagios3 nagios-nrpe-plugin
-# echo "vagrant" | htpasswd -i /etc/nagios3/htpasswd.users nagiosadmin
-# usermod -a -G nagios www-data
-# sed -i 's/check_external_commands=0/check_external_commands=1' /etc/nagios3/nagios.cfg
-# service apache2 restart
-# service nagios3 restart
+cmk -I # inventory - find new services
+cmk -R # precompile + config + core restart
