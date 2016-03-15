9cf0cfd61528010af2179c05d9daf1b0856643a5
Mike Morrow <mmorrow@vt.edu>
2016-03-15 15:56:26 -0400

    Minor syntax fix.

--- scripts/centos/installCheckmkServer.sh
+++ scripts/centos/installCheckmkServer.sh
@@ -6,7 +6,8 @@ curl -L -O $checkMKrpm
 yum install --nogpgcheck check-mk-raw-1.2.6p16-el7-34.x86_64.rpm -y
 /usr/sbin/setsebool httpd_can_network_connect 1
 omd create dev
-su - dev -c "omd start"
+#su - dev -c "omd start"
+omd start dev
 /bin/cp /vagrant/conf/check_mk/main.mk /opt/omd/sites/dev/etc/check_mk
 su - dev -c "cmk -I" # inventory - find new services
 su - dev -c "cmk -R" # precompile + config + core restart
