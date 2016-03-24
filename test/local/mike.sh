8b874fe8d5a6101f98957af4260de2dccdc8bea3
Mike Morrow <mmorrow@vt.edu>
2016-03-24 15:15:17 -0400

    add test flag to flap status

--- test/local/mike.sh
+++ test/local/mike.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-value=$(($(date +%s) % 600 ))
-
+checkLocation=$(dirname $(readlink -f $0))
+echo
+if [ -e ${checkLocation}/flap ]; then
+  value=$(($(date +%s) % 600 ))
+else
+  value=$(($(date +%s) % 250 ))
+fi
 echo "P MikeCheck var1=${value};300;450"
