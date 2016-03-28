7cfe7f265e82d3fd180fc361ddbf688cce1fc948
Mike Morrow <mmorrow@vt.edu>
2016-03-28 13:34:47 -0400

    Added logwatch config and test scripts

--- scripts/centos/installCheckmkClient.sh
+++ scripts/centos/installCheckmkClient.sh
@@ -13,3 +13,8 @@ systemctl start xinetd.service
 systemctl enable xinetd.service
 #firewall-cmd --add-port=6556/tcp --permanent
 #firewall-cmd --reload
+# ---------------------------------------------------------------------------
+# Install logwatch agent
+# ---------------------------------------------------------------------------
+/bin/cp /vagrant/conf/check_mk/mk_logwatch /usr/share/check-mk-agent/plugins/
+/bin/cp /vagrant/conf/check_mk/logwatch.cfg /etc/check-mk-agent/
