c0683c88fb41cb48d913cb1831d084e0c5e52586
Mike Morrow <mmorrow@vt.edu>
2016-04-27 16:55:19 -0400

    Update sebooleans for centralServer

--- scripts/centos/installCheckmkCentral.sh
+++ scripts/centos/installCheckmkCentral.sh
@@ -4,7 +4,7 @@ export checkMKrpm=https://mathias-kettner.de/support/1.2.6p16/check-mk-raw-1.2.6
 yum install epel-release -y
 curl -L -O $checkMKrpm
 yum install --nogpgcheck check-mk-raw-1.2.6p16-el7-34.x86_64.rpm -y
-/usr/sbin/setsebool httpd_can_network_connect 1
+/usr/sbin/setsebool -P httpd_can_network_connect 1
 # Copy local test scripts to check_mk local directory
 cp /vagrant/test/local/* /usr/share/check-mk-agent/local
 omd create dev
